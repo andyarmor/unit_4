c22664dee1f14ef7ce7c6213ef8887324b4aab41
Andy Armor <aoarmor25@students.bmchs.orggit config --global user.name Andy>
2022-11-28 14:44:25 -0600

    not done

--- tictactoe.py
+++ tictactoe.py
@@ -1,4 +1,4 @@
-board = [['','',''], ['','',''], ['', '', '']]
+board = [[' ',' ',' '], [' ',' ',' '], [' ', ' ', ' ']]
 player_1 = input("Player 1, what is your name? ")
 player_1_symbol = 'X'
 player_2 = input("Player 2, what is your name? ")
@@ -11,18 +11,19 @@ def print_board():
     print()
     row_count = 1
     for row in board:
+        
         row_string = ""
         for i in range(0,3):
             if i < 2:
                 row_string += str(row[i]) + " |"
             else:
                 row_string +=str(row[i])
-        if row_count < 4: 
+        if row_count < 3: 
             print(row_string)
-            print('------')
+            print('--------')
             row_count +=1
         else:
-            print(row_count)
+            print(row_string)
 
 def check_tied(board):
 
@@ -34,7 +35,7 @@ def make_move(board, name, symbol):
         print(f"{name}, where would you like your marker?")
         player_column = int(input(" Enter a column 1-3: "))
         player_row = int(input(" Enter a row 1-3: "))
-        if board[player_row -1][player_column-1] == '':
+        if board[player_row -1][player_column-1] == ' ':
             board[player_row -1][player_column-1] = symbol
             valid_move = True
         if not valid_move:
@@ -42,9 +43,22 @@ def make_move(board, name, symbol):
 
 
 
-def check_winner(gameboard):
+def check_winner(board):
 
-    pass
+    winner = ''
+
+    #check the rows and collumns for win
+    for i in range(3):
+        if board[0][i] == board[1][i] and board[1][i] == board[2][i]:
+            winner = board[i][0]
+        elif board[i][0] == board[i][1] and board[i][1] == board[i][2]:
+            winner = board[0][i]
+        
+    if board[0][0] == board[1][1] and board[1][1] == board[2][2]:
+        winner = board[0][0]
+    elif board[0][2] == board[1][1] and board[1][1] == board [2][0]:
+        winner = board[0][2]
+    return winner
 
     
 
@@ -55,3 +69,5 @@ while True:
     make_move(board, player_1, player_1_symbol)
 
     make_move(board, player_2, player_2_symbol)
+
+    winner = check_winner(board)

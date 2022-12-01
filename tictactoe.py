e8ced5e198c1a10599ad5dc8d7d92712d77582ac
andyarmor <aoarmor25@students.bmchs.org>
2022-11-30 22:03:21 -0600

    not done

--- tictactoe.py
+++ tictactoe.py
@@ -48,11 +48,11 @@ def make_move(name, symbol):
 def end_game(board):
 
     game_winner = ''
-    
-    if board[0][0] == board[0][1] and board[0][1] == board[0][2]:
-        game_winner = board[0][0]
-    elif board[0][0] == board[1][0] and board[1][0] == board[2][0]:
-        game_winner = board[0][0]
+    for i in range(3):
+        if board[i][0] == board[i][1] and board[i][1] == board[i][2]:
+            game_winner = board[i][0]
+        elif board[0][i] == board[1][i] and board[1][i] == board[2][i]:
+            game_winner = board[0][0]
     if board[0][0] == board[1][1] and board[1][1] == board[2][2]:
         game_winner = board[0][0]
     elif board[0][2] == board[1][1] and board[1][1] == board[2][0]:
